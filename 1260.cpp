03211b5c1a55116a63fe05838cb99c5b1ba62e61
baekminsu <chodinggame@naver.com>
2024-01-09 19:30:47 +0900

    240109D1

---
+++ 1260.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> vArr[1000];
+int visit[1000] = { 0, };
+
+void dfs(int node) {
+	visit[node] = 1;
+	cout << node << " ";
+	for(int i = 0; i < vArr[node].size(); i++) {
+		int nextNode = vArr[node][i];
+		if (visit[nextNode] == 1) {
+			continue;
+		}
+		dfs(nextNode);
+	}
+}
+
+int main() {
+	int N, M, V;
+	cin >> N >> M >> V;
+	int V1, V2;
+	for (int i = 0; i < M; i++) {
+		cin >> V1 >> V2;
+		vArr[V1].push_back(V2);
+		vArr[V2].push_back(V1);
+	}
+	dfs(V);
+	cout << "\n";
+	return 0;
+}

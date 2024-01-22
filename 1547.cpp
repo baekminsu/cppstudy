f66bd475122f110ed5d83489eae5ab797c46753d
baekminsu <110341040+baekminsu@users.noreply.github.com>
2024-01-22 23:37:44 +0900

    Create 1547.cpp
    error

---
+++ 1547.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int M;
+	cin >> M;
+
+	int cup[3] = { 1, };
+	int X, Y;
+	int temp;
+	for (int i = 0; i < M; i++) {
+		cin >> X >> Y;
+		X = X - 1; Y = Y - 1;
+		temp = cup[Y];
+		cup[Y] = cup[X];
+		cup[X] = temp;
+	}
+	for (int i = 0; i < M; i++) {
+		if (cup[i] == 1) {
+			cout << i+1;
+		}
+	}
+
+
+	return 0;
+}

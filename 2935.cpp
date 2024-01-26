3fc33884482f02d2a2730d30e95d5383c550cc1b
baekminsu <chodinggame@naver.com>
2024-01-26 19:48:59 +0900

    baek2935ErrorD2

--- 2935.cpp
+++ 2935.cpp
@@ -1,61 +1,68 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main() {
 
-	string A, B;
-	char oper;
+	string A;
+	char com;
+	string B;
 
-	cin >> A >> oper >> B;
-
-	if (stoi(A) + stoi(B) == 2) {
-		if (oper == '+') {
-			cout << "2";
-		}
-		else if (oper == '*') {
+	cin >> A >> com >> B;
+	int save;
+	if (com == '*') {
+		if (stoi(A) * stoi(B) == 1) {
 			cout << "1";
+			return 0;
 		}
-	}
-	else if (A.length() > B.length()) {
-		if (oper == '*') {
-			if (B.length() > 1) {
-				for (int i = 0; i < B.length() - 1; i++) {
-					A.push_back('0');
-				}
+		else if (A.length() >= B.length()) {
+			for (int i = 0; i < B.length() - 1; i++) {
+				A.push_back(0);
 			}
+			cout << A;
+			return 0;
 		}
-		else if (oper == '+') {
-			A[A.length() - B.length()] = '1';
-		}
-		cout << A;
-	}
-	else if (A.length() < B.length()){
-		if (oper == '*') {
-			if (A.length() > 1) {
-				for (int i = 0; i < A.length() - 1; i++) {
-					B.push_back('0');
-				}
+		else if (A.length() < B.length()) {
+			for (int i = 0; i < A.length() - 1; i++) {
+				B.push_back(0);
 			}
+			cout << B;
+			return 0;
 		}
-		else if (oper == '+') {
-			B[B.length() - A.length()] = '1';
-		}
-		cout << B;
+
+
 	}
-	else { // 자리수가 같을때
-		if (oper == '*') {
-			if (B.length() > 1) {
-				for (int i = 0; i < B.length() - 1; i++) {
-					A.push_back('0');
-				}
-			}
-		}
-		else if (oper == '+') {
+	else if (com == '+') {
+		if (A.length() == B.length()) {
 			B[0] = '2';
+			cout << B;
+			return 0 ;
 		}
-		cout << B;
+		else if (A.length() > B.length()) {
+			for (int i = 0; i < B.length(); i++) {
+				if (B[i] == 1) {
+					save = i;
+					break;
+				}
+			}
+			A[save] = 1;
+			cout << A;
+			return 0;
+		}
+		else if (A.length() < B.length()) {
+			for (int i = 0; i < A.length(); i++) {
+				if (A[i] == 1) {
+					save = i;
+					break;
+				}
+			}
+			B[save] = 1;
+			cout << B;
+			return 0;
+		}
 	}
+
+
+
 
 	return 0;
 }

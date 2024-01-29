b0ef632b56327d5905446e35fc3227c1da93a989
baekminsu <chodinggame@naver.com>
2024-01-29 19:14:09 +0900

    baek2935d3

--- 2935.cpp
+++ 2935.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
 
 	string A;
@@ -9,21 +10,22 @@ int main() {
 
 	cin >> A >> com >> B;
 	int save;
+
 	if (com == '*') {
-		if (stoi(A) * stoi(B) == 1) {
+		if (A.length() == 1 && B.length() == 1) {
 			cout << "1";
 			return 0;
 		}
 		else if (A.length() >= B.length()) {
 			for (int i = 0; i < B.length() - 1; i++) {
-				A.push_back(0);
+				A.push_back('0');
 			}
 			cout << A;
 			return 0;
 		}
 		else if (A.length() < B.length()) {
 			for (int i = 0; i < A.length() - 1; i++) {
-				B.push_back(0);
+				B.push_back('0');
 			}
 			cout << B;
 			return 0;
@@ -39,30 +41,37 @@ int main() {
 		}
 		else if (A.length() > B.length()) {
 			for (int i = 0; i < B.length(); i++) {
-				if (B[i] == 1) {
+				if (B[i] == '1') {
 					save = i;
 					break;
 				}
 			}
-			A[save] = 1;
+			if (A[A.length() -B.length()] == '1') {
+				A[save] = '2';
+			}
+			else {
+				A[A.length() - B.length()] = '1';
+			}
 			cout << A;
 			return 0;
 		}
 		else if (A.length() < B.length()) {
 			for (int i = 0; i < A.length(); i++) {
-				if (A[i] == 1) {
+				if (A[i] == '1') {
 					save = i;
 					break;
 				}
 			}
-			B[save] = 1;
+			if (B[B.length() - A.length()] == '1') {
+				A[save] = '2';
+			}
+			else {
+				B[B.length() - A.length()] = '1';
+			}
 			cout << B;
 			return 0;
 		}
 	}
-
-
-
 
 	return 0;
 }
